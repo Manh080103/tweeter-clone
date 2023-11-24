3961f2ae4bac40d1d533b9b1df0af8a794589d4a
Manh Nguyen <ducmanh080103@gmail.com>
2023-11-24 17:57:29 +0700

    prisma generate

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,62 @@
+import { signOut } from "next-auth/react";
+
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
+import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
+import SidebarTweetButton from "./SidebarTweetButton";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+const Sidebar = () => {
+  const { data: currentUser } = useCurrentUser();
+
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notifications",
+      href: "/notifications",
+      icon: BsBellFill,
+      auth: true,
+    },
+    {
+      label: "Profile",
+      href: `/users/${currentUser?.id}`,
+      auth: true,
+      icon: FaUser,
+    },
+  ];
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+              auth={item.auth}
+            />
+          ))}
+          {currentUser && (
+            <SidebarItem
+              onClick={() => signOut()}
+              icon={BiLogOut}
+              label="Log out"
+            />
+          )}
+
+          <SidebarTweetButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
